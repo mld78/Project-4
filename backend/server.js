f057e8fde3ed7ea77565870efa7404e8c4d103b3
Matt Dias <mld78@case.edu>
2017-04-10 11:29:57 -0700

    added backend stuff for now

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,34 @@
-var
-	express = require('express'),
-	app = express(),
-	logger = require('morgan'),
-	favicon = require('serve-favicon')
+var express = require('express'),
+	  app = express(),
+	  logger = require('morgan'),
+	  favicon = require('serve-favicon'),
+    cors = require('cors'),
+    bodyParser = require('body-parser'),
+    path = require('path')
+
+  var mongoose = require('mongoose')
+
+
+  var routes = require('./config/routes')
+
+app.use(cors())
+
+
+
+
+
 
 app.use(logger('dev'))
 app.use(express.static('frontend'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 // app.use(favicon(__dirname + '/public/img/favicon.ico'))
+app.use(routes)
+
+
+
+
+
 
 app.listen(3000, function(err) {
 	if(err) return console.log(err)
