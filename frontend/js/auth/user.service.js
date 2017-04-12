4b46eaa1c6ab3d8408a0bc132b3a63432c8e4d26
Matt Dias <mld78@case.edu>
2017-04-12 16:46:44 -0700

    attempt to fix things

--- frontend/js/auth/user.service.js
+++ frontend/js/auth/user.service.js
@@ -14,7 +14,7 @@ angular.module('LoLStats')
     function create(data) {
       var promise = $http({
         method: 'POST',
-        url:    'http://localhost:3000/api/users',
+        url:    '/api/users',
         data:   data
       });
 
