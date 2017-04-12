12e94568d87e57182d8bad5d27d1b1d6a2592793
Matt Dias <mld78@case.edu>
2017-04-12 09:00:13 -0700

    idk

--- frontend/js/runesController.js
+++ frontend/js/runesController.js
@@ -7,16 +7,6 @@
 	function RunesController(RuneFactory){
 		var self = this
 
-    self.isCollapsed1 = true
-    self.isCollapsed2 = true
-    self.isCollapsed3 = true
-    self.isCollapsed4 = true
-
-
-    console.log("Runes controller accessed...")
-		// self.all = []
-    self.runeList = []
-
     RuneFactory.show()
     .success(function(data) {
       for(rune in data.data) {
@@ -25,24 +15,89 @@
       console.log(self.runeList)
       console.log("Testing 123")
 
-		})
-
-    //functions that lists runes by tiers
-    // function selectTier(){
-    //   if (self.runeList.rune.tier == 1){
-    //     return self.runeList.rune.filter(function(x){ x.tier == 1})
-    //   } else if(self.runeList.rune.tier == 2){
-    //     return self.runeList.rune.filter(function(x){ x.tier == 2})
-    //   } else {
-    //     return self.runeList.rune.filter(function(x){x.tier == 3})
-    //   }
-    // }
-
-
-    //function that groups runes by type
+    })
 
 
 
-    //function that adds the attributes of the runes
+    //toggle buttons in select rune list
+    self.isCollapsed1 = true
+    self.isCollapsed2 = true
+    self.isCollapsed3 = true
+    self.isCollapsed4 = true
+
+
+    //set max number of items in selected rune list
+    self.maxMarks = 9
+    self.maxGlyphs = 9
+    self.maxSeals = 9
+    self.maxQuints = 4
+
+    console.log("Runes controller accessed...")
+		// self.all = []
+    self.runeList = []
+
+
+
+
+
+
+//move a selected mark from the available mark list to the selected runes list
+self.selectedMark = {}
+self.setMark = setMark
+self.totalMarks =[]
+
+
+function setMark(rune){
+  self.selectedMark = rune
+  if(self.totalMarks.length<9){
+    self.totalMarks.push(self.selectedMark)
+  }
+  console.log(self.selectedMark)
+}
+
+//move a selected glyph from the available glyph list to the selected runes list
+self.selectedGlyph = {}
+self.setGlyph = setGlyph
+self.totalGlyphs =[]
+
+
+function setGlyph(rune){
+  self.selectedGlyph = rune
+  if (self.totalGlyphs.length<9){
+  self.totalGlyphs.push(self.selectedGlyph)
+}
+  console.log(self.selectedGlyph)
+}
+
+//move a selected seal from the available seal list to the selected runes list
+self.selectedSeal = {}
+self.setSeal = setSeal
+self.totalSeals =[]
+
+
+function setSeal(rune){
+  self.selectedSeal = rune
+  if (self.totalSeals.length < 9){
+  self.totalSeals.push(self.selectedSeal)
+}
+  console.log(self.selectedSeal)
+}
+
+//move a selected quint from the available quint list to the selected runes list
+self.selectedQuint = {}
+self.setQuint = setQuint
+self.totalQuints =[]
+
+
+function setQuint(rune){
+  self.selectedQuint = rune
+  if (self.totalQuints.length<3){
+  self.totalQuints.push(self.selectedQuint)
+}
+  console.log(self.selectedQuint)
+}
+
+
+
 
   }
